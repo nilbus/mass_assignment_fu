daf6bc5161c92b774e1cbd15fb42efce3bf0fd1a
Edward Anderson <nilbus@nilbus.com>
2010-06-22 16:42:52 -0400

    Passing rspec tests for attribute_fu.

--- spec/attribute_fu_spec.rb
+++ spec/attribute_fu_spec.rb
@@ -1,10 +1,36 @@
 require File.dirname(__FILE__) + '/spec_helper'
-require File.dirname(__FILE__) + '/lib/attribute_fu'
 
-describe "AttributeFu"
+describe MockModel do
+  it "should initialize attributes" do
+    Student.new.attributes.should == Student.new.initial_attributes
+    Grade.new.attributes.should == Grade.new.initial_attributes
+    Profile.new.attributes.should == Profile.new.initial_attributes
+  end
+end
+
+describe "AttributeFu" do
   before :each do
     @student = Student.new
   end
 
-  it "should" do
+  it "should protect immediate attributes" do
+    @student.update_attributes_for :teacher, { :full_name => 'Jeff Joxworthy', :preferred_name => 'foxxy' }
+    @student.attributes.should == @student.initial_attributes
   end
+
+  it "should allow changes to immediate attributes" do
+    @student.update_attributes_for(:student, { :preferred_name => 'Jeff' })
+    @student.attributes.should == @student.initial_attributes.merge({'preferred_name' => 'Jeff'})
+  end
+
+  it "should allow updating and protect nested models" do
+    @student.update_attributes_for(:administrator, { :preferred_name => 'Jeffery Fensworth', :grades_attributes => { '1' => Grade.new.initial_attributes.merge('letter_grade' => 'A', 'override_letter_grade' => 'A') } })
+    @student.attributes.should == @student.initial_attributes.merge({'grades_attributes' => { '1' => Grade.new.initial_attributes.merge('override_letter_grade' => 'A') } })
+  end
+
+  it "should give access to all attributes when :all is specified" do
+    @student.update_attributes_for(:student, { :profile_attributes => Profile.new.initial_attributes.merge('favorite_sport' => 'world cup') })
+    @student.attributes.should == @student.initial_attributes.merge({'profile_attributes' => Profile.new.initial_attributes.merge('favorite_sport' => 'world cup') })
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,18 +4,90 @@ rescue LoadError
   puts "You need to install rspec in your base app"
   exit
 end
-require File.dirname(__FILE__) + '/../../../../vendor/plugins/lib/attribute_fu'
+require File.dirname(__FILE__) + '/../../../../vendor/plugins/attribute_fu/lib/attribute_fu'
 
 plugin_spec_dir = File.dirname(__FILE__)
 ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
 
 class MockModel
-  attr_accessor :attributes
-  attr_accessor :accessible_attributes
+  cattr_accessor :accessible_attributes
+
+  def attributes=(attr, check_accessible=nil)
+    @attributes ||= {}
+    @attributes.recursive_merge! attr if attr
+  end
+
+  def attributes
+    @attributes
+  end
+
+  def save; end
+
+  def attributes_protected_by_default; ['id']; end
+
+  def log_protected_attribute_removal(filtered_attributes)
+    @log ||= []
+    @log << filtered_attributes
+  end
+
+  def self.reflect_on_association(association)
+    $assoc = association.to_s.singularize.classify
+    return self
+  end
+
+  def self.klass
+    Object.const_get $assoc rescue nil
+  end
 end
 
 class Student < MockModel
   include AttributeFu
 
+  nested_attr_accessible_for :administrator, [:full_name, { :grades_attributes => [:override_letter_grade] }]
+  nested_attr_accessible_for :student, [:preferred_name, { :profile_attributes => :all }]
+  nested_attr_accessible_for :teacher, :grades_attributes
 
+  def initial_attributes
+    { 'full_name' => 'Jeff Fenworth', 'preferred_name' => 'Jef',
+      'grades_attributes' => { '1' => Grade.new.initial_attributes },
+      'profile_attributes' => Profile.new.initial_attributes }
+  end
+
+  def initialize
+    self.attributes = initial_attributes
+  end
+end
+
+class Grade < MockModel
+  include AttributeFu
+
+  def initial_attributes
+    { 'letter_grade' => 'C', 'class_id' => '3857', 'override_letter_grade' => nil }
+  end
+
+  def initialize
+    self.attributes = initial_attributes
+  end
+
+  def self.accessible_attributes
+    ['class_id']
+  end
+end
+
+class Profile < MockModel
+  include AttributeFu
+
+  def initial_attributes
+    { 'favorite_sport' => 'quiddich' }
+  end
+
+  def initialize
+    self.attributes = initial_attributes
+  end
+end
+
+class Hash
+  def recursive_merge!(h)
+    self.merge!(h) {|key, _old, _new| if _old.class == Hash then _old.recursive_merge!(_new) else _new end  }
+  end
 end
